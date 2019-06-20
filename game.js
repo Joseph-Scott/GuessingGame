870584ae1e9aa73827856087d66628406112f7ea
Joseph Scott <jbs198585@gmail.com>
2019-06-20 05:39:41 -0400

    solution

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
-alert("HELLO!");
+//create secretNumber
+var secretNumber = 4
+
+//ask user for guess
+var stringGuess = prompt("Guess a number");
+var guess = Number(stringGuess);
+
+//check if guess is right
+if(guess === secretNumber) {
+	alert("YOU GOT IT RIGHT!");
+}
+
+//check if guess is higher
+else if (guess > secretNumber) {
+	alert("Too high, guess again!");
+}
+
+//Otherwise, check if lower
+
+else {
+	alert("Too low, guess again!");
+}
